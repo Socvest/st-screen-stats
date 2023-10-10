6848f4518508a02404f73a3dbcd6a462bf89515a
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-10 13:50:06 +0100

    Update example.py

--- st_screen_stats/example.py
+++ st_screen_stats/example.py
@@ -1,16 +1,20 @@
 import streamlit as st
-from __init__ import st_screen_data
+from st_screen_stats import ScreenData, StreamlitNativeWidgetScreen
 
-# Add some test code to play with the component while it's in development.
-# During development, we can run this just as we would any other Streamlit
-# app: `$ streamlit run my_component/example.py`
 
 st.subheader("Component with constant args")
 
-# Create an instance of our component with a constant `name` arg, and
-# print its output value.
+screenD = ScreenData()
+screen_d = screenD.st_screen_data_window_top()
 
-num_clicks = st_screen_data()
-st.write(num_clicks)
+st.write(screen_d)
+
+
+st.subheader("native widget method")
+screenDN = StreamlitNativeWidgetScreen()
+screenDN.st_screen_data_window_top()
+stats_ = screenDN.get_window_screen_stats()
+st.write(stats_)
+
 
 
