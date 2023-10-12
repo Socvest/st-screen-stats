78901863449d14657fda7559a3c0cd7f5d2cd6af
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-12 17:52:50 +0100

    Add files via upload

---
+++ st_screen_stats/SizeRange/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import WindowScreenRange from "./WindowScreenRange"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WindowScreenRange />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
