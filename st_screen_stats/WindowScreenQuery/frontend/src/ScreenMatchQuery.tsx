0acc1b3bc11bbc3313597cbe39e866a992a0cd9c
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-13 01:16:40 +0100

    Update ScreenMatchQuery.tsx

--- st_screen_stats/WindowScreenQuery/frontend/src/ScreenMatchQuery.tsx
+++ st_screen_stats/WindowScreenQuery/frontend/src/ScreenMatchQuery.tsx
@@ -11,28 +11,81 @@ const ScreenMatchQuery: React.FC<ComponentProps> = (props) => {
 
   const { args } = props
   let mediaMatchQ:any = args["mediaMatchQ"]
+  let windowType:any = args["windowType"]
 
-  const [mediaMatch, setMediaMatch] = useState({status:false})
+  const [mediaMatch, setMediaMatch] = useState<any>({status:false})
+
+  useEffect(() => {
+
+    switch (windowType) {
+      case "window":
+        let mediaMatch_ = window.matchMedia(mediaMatchQ).matches;
+        mediaMatch.status = mediaMatch_
+        setMediaMatch(mediaMatch)
+        Streamlit.setComponentValue({status:mediaMatch_})
+        Streamlit.setComponentReady()
+        break
+      case "windowTop":
+        let mediaMatch_2 = window.top?.matchMedia(mediaMatchQ).matches
+        mediaMatch.status = mediaMatch_2
+        setMediaMatch(mediaMatch)
+        Streamlit.setComponentValue({status:mediaMatch_2})
+        Streamlit.setComponentReady()
+        break
+      default:
+        break
+    }
+
+  }, [])
 
   useEffect(() => {
     let matchQueryWindow = window.matchMedia(mediaMatchQ);
 
     const MediaMatchWindow = () => {
-      let mediaMatch_ = window.matchMedia(mediaMatchQ).matches;
-      mediaMatch.status = mediaMatch_;
-      setMediaMatch(mediaMatch);
-      Streamlit.setComponentValue(mediaMatch)
-      Streamlit.setComponentReady()
+      switch (windowType) {
+        case "window":
+          let mediaMatch_1 = window.matchMedia(mediaMatchQ).matches;
+          mediaMatch.status = mediaMatch_1;
+          setMediaMatch(mediaMatch);
+          Streamlit.setComponentValue(mediaMatch)
+          Streamlit.setComponentReady()
+          break;
+        default:
+          break;
+      }
+    };
+    matchQueryWindow.addEventListener("change", MediaMatchWindow);
+
+    return () => {
+      matchQueryWindow.removeEventListener("change", MediaMatchWindow);
+    };
+  }, [mediaMatch]);
+
+  
+  useEffect(() => {
+    let matchQueryWindow = window.top?.matchMedia(mediaMatchQ)
+
+    const MediaMatchWindow = () => {
+      switch (windowType) {
+        case "windowTop":
+          let mediaMatch_2 = window.top?.matchMedia(mediaMatchQ).matches
+          mediaMatch.status = mediaMatch_2;
+          Streamlit.setComponentValue(mediaMatch)
+          Streamlit.setComponentReady()
+          break;
+        default:
+          break;
+      }
     };
 
-    
-    
-    matchQueryWindow.addEventListener("change", MediaMatchWindow)
-    
+
+    matchQueryWindow?.addEventListener("change", MediaMatchWindow);
+
     return () => {
-      matchQueryWindow.removeEventListener("change", MediaMatchWindow)
-    }
-  }, [mediaMatch])
+      matchQueryWindow?.removeEventListener("change", MediaMatchWindow);
+    };
+  }, [mediaMatch]);
+
 
   return (
     <div style={{display:"none"}}></div>
