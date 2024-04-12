1936f219bf7ac248364556adacbe48f16f3953b5
Socvest <82335390+Socvest@users.noreply.github.com>
2024-04-12 12:49:13 +0100

    Add files via upload

--- st_screen_stats/WindowScreenQuery/frontend/src/ScreenMatchQuery.tsx
+++ st_screen_stats/WindowScreenQuery/frontend/src/ScreenMatchQuery.tsx
@@ -10,44 +10,40 @@ import React, { ReactNode, useState, useEffect } from "react"
 const ScreenMatchQuery: React.FC<ComponentProps> = (props) => {
 
   const { args } = props
-  let mediaMatchQ:any = args["mediaMatchQ"]
-  let windowType:any = args["windowType"]
-
-  const [mediaMatch, setMediaMatch] = useState<any>({status:false})
+  let mediaMatchQ: any = args["mediaMatchQ"]
+  let windowType: any = args["windowType"]
 
   useEffect(() => {
 
     switch (windowType) {
       case "window":
         let mediaMatch_ = window.matchMedia(mediaMatchQ).matches;
-        mediaMatch.status = mediaMatch_
-        setMediaMatch(mediaMatch)
-        Streamlit.setComponentValue({status:mediaMatch_})
+
+        Streamlit.setComponentValue({ status: mediaMatch_ })
         Streamlit.setComponentReady()
         break
       case "windowTop":
         let mediaMatch_2 = window.top?.matchMedia(mediaMatchQ).matches
-        mediaMatch.status = mediaMatch_2
-        setMediaMatch(mediaMatch)
-        Streamlit.setComponentValue({status:mediaMatch_2})
+
+        Streamlit.setComponentValue({ status: mediaMatch_2 })
         Streamlit.setComponentReady()
         break
       default:
         break
     }
 
+
   }, [])
 
   useEffect(() => {
+
     let matchQueryWindow = window.matchMedia(mediaMatchQ);
 
     const MediaMatchWindow = () => {
       switch (windowType) {
         case "window":
           let mediaMatch_1 = window.matchMedia(mediaMatchQ).matches;
-          mediaMatch.status = mediaMatch_1;
-          setMediaMatch(mediaMatch);
-          Streamlit.setComponentValue(mediaMatch)
+          Streamlit.setComponentValue({ status: mediaMatch_1 })
           Streamlit.setComponentReady()
           break;
         default:
@@ -59,9 +55,9 @@ const ScreenMatchQuery: React.FC<ComponentProps> = (props) => {
     return () => {
       matchQueryWindow.removeEventListener("change", MediaMatchWindow);
     };
-  }, [mediaMatch]);
+  }, []);
 
-  
+
   useEffect(() => {
     let matchQueryWindow = window.top?.matchMedia(mediaMatchQ)
 
@@ -69,8 +65,7 @@ const ScreenMatchQuery: React.FC<ComponentProps> = (props) => {
       switch (windowType) {
         case "windowTop":
           let mediaMatch_2 = window.top?.matchMedia(mediaMatchQ).matches
-          mediaMatch.status = mediaMatch_2;
-          Streamlit.setComponentValue(mediaMatch)
+          Streamlit.setComponentValue({ status: mediaMatch_2 })
           Streamlit.setComponentReady()
           break;
         default:
@@ -84,11 +79,11 @@ const ScreenMatchQuery: React.FC<ComponentProps> = (props) => {
     return () => {
       matchQueryWindow?.removeEventListener("change", MediaMatchWindow);
     };
-  }, [mediaMatch]);
+  }, []);
 
 
   return (
-    <div style={{display:"none"}}></div>
+    <div style={{ display: "none" }}></div>
   )
 }
 
