5fa2698086223453924de5b072df54d330b58b5c
Socvest <82335390+Socvest@users.noreply.github.com>
2024-04-12 12:43:56 +0100

    Add files via upload

--- st_screen_stats/ScreenData/__init__.py
+++ st_screen_stats/ScreenData/__init__.py
@@ -1,5 +1,7 @@
 import os
+import time
 import streamlit as st
+from typing import Literal, Optional, Union
 import streamlit.components.v1 as components
 from st_screen_stats import IS_RELEASE
 
@@ -28,19 +30,29 @@ class ScreenData:
             - setTimeout: this dictates period pause until data is received after resize. Default is 1000 (1 second)
     """
 
-    def __init__(self, setTimeout=1000) -> None:
+    def __init__(self, setTimeout:Union[int,float]=1000, useNativePause:bool=True) -> None:
+        """
+            - setTimeout: amount of time for component to wait before sending traffic/data in ms default is 1000ms (1 sec).
+        """
         self.setTimeout = setTimeout
+        self.useNativePause = useNativePause
         
-
-    def st_screen_data_window(self, default=None, key=None):
+        
+    def st_screen_data_window(self, default=None, key=None): 
         """
         Component to recieve screen stats to help you build a more responsive app.
         - default: default value for component 
         - key: unique identifier for streamlit (will be recognised by streamlit session state)
         """
-        component_value = _st_screen_data(windowType="window", setTime=self.setTimeout, key=key, default=default)
 
+        component_value = _st_screen_data(windowType="window", setTime=self.setTimeout, key=key, default=default) 
+        if self.useNativePause:
+            if self.setTimeout != None and isinstance(self.setTimeout, (int, float)): 
+                pause = (self.setTimeout/1000) + 1
+                time.sleep(pause) 
+                return component_value
         return component_value
+        
     
     def st_screen_data_window_top(self, default=None, key=None):
         """
@@ -49,5 +61,10 @@ class ScreenData:
         - key: unique identifier for streamlit (will be recognised by streamlit session state)
         """
         component_value = _st_screen_data(windowType="windowTop", setTime=self.setTimeout, key=key, default=default)
+        if self.useNativePause:
+            if self.setTimeout != None and isinstance(self.setTimeout, (int, float)): 
+                pause = (self.setTimeout/1000) + 1
+                time.sleep(pause) 
+                return component_value 
 
         return component_value
